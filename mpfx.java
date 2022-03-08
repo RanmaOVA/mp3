96dad79e4262f8144f5bf821cc6dc169e87409f8
RanmaOVA <Masseymichael643@gmail.com>
2022-03-07 23:08:07 -0600

    starting code

---
+++ mpfx.java
@@ -0,0 +1,9 @@
+// *************************
+// JavaFX mp3 music player
+// *************************
+package application;
+
+import java.io.File;
+ 
+public class Controller implements Initializable{
+    {
